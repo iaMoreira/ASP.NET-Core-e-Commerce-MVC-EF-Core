90cf104c051e8013f221643e2ec777bd0c7d2c45
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-03-31 23:46:48 -0300

    feature: Banco de Dados

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 namespace CasaDoCodigo
 {
     public class Startup
@@ -24,10 +25,12 @@ namespace CasaDoCodigo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            string connectionString = Configuration.GetConnectionString("Default");
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -52,6 +55,8 @@ namespace CasaDoCodigo
                     name: "default",
                     pattern: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
         }
     }
 }
